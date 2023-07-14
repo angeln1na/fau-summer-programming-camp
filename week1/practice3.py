74737be9d2ec6c73f297fb65c7f8f07122e9f27a
angeln1na <123002085+angeln1na@users.noreply.github.com>
2023-07-14 09:31:42 -0400

    Create practice3.py

---
+++ week1/practice3.py
@@ -0,0 +1,13 @@
+#Display to the screen
+print("The weather in Florida can be confusing. Its important to know the weather everyday")
+#Ask the user about the weather
+weather= int(input("What is the weather?"))
+#start comparing the weather with degrees
+if weather <= 40:
+#print statement that returns the weather
+print ("The weather is",weather, "degrees fahrenheit.")
+#print statement dependent on the number the user inputs
+print("Its pretty cold you should wear a jacket.")
+#1. Continue this program and check different cases from 10 through 100 degrees
+#2. Write elif statements for each value of 10 - 100
+#3. Write a print statements for each case
