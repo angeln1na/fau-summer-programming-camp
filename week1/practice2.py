bccd10044deb2b92d0046b82d3b3f8c3b3f6d3ef
angeln1na <123002085+angeln1na@users.noreply.github.com>
2023-07-14 09:29:12 -0400

    Add files via upload

---
+++ week1/practice2.py
@@ -0,0 +1,13 @@
+#X input variable to enter a number
+x=input("Enter a number:")
+#Y input variable to enter a number
+y=input("Enter a number:")
+#A variable to calculate addition of both integer variables
+addition = int(x)+int(y)
+#Displaying Addition to the screen
+print("Addition")
+#Displaying "X + Y =" and returning the corresponding variable
+print(x, "+", y, "=", addition)
+
+# 1. Following the example continue with creating variables for subtraction, multiplication, division and square root
+# 2. Add two more variables to input such as a and b calculate them separately as X and Y
