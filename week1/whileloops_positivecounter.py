449daa816bf70736a1126c9b512efc985ce2a605
angeln1na <123002085+angeln1na@users.noreply.github.com>
2023-07-14 12:39:52 -0400

    Create whileloops_positivecounter.py

---
+++ week1/whileloops_positivecounter.py
@@ -0,0 +1,9 @@
+#Program will get a positive number from user and count up to the number from 0
+
+#1. Get a positive number from user and store it in a variable
+#2. Set a variable, counter, to 0
+#3. Use a while loop to check if user_number is less than or equal to 0
+    #4. If it is less than or equal to 0, print out "Please enter a positive number" (input statement) and store it in same variable from question 1
+#5. Set a while loop while user_number is greater than 0
+    #6. Add counter to user_number
+    #7. Print out counter
