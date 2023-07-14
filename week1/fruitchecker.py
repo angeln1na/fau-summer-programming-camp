a5a15613e98ca38af9db08f089efba297dcd69e8
angeln1na <123002085+angeln1na@users.noreply.github.com>
2023-07-14 09:33:23 -0400

    Create fruitchecker.py

---
+++ week1/fruitchecker.py
@@ -0,0 +1,6 @@
+#Exercise for logical operators and if-elif statements
+#Prompt the user for a fruit name
+#Check if the fruit is a tropical or citrus fruit
+#If the fruit is “mango” or “pineapple”, then print out “It’s a tropical fruit!”
+#If the fruit is “orange” or “lemon”, then print out “It’s a citrus fruit!”
+#HINT: use .lower() function when checking the fruit cases, example: fruit.lower()
