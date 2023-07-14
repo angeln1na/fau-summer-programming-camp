d2e6d01671ab13351d2c6a00efc5dec3793994e7
angeln1na <123002085+angeln1na@users.noreply.github.com>
2023-07-14 09:39:04 -0400

    Create lists_exercise.py

---
+++ week1/lists_exercise.py
@@ -0,0 +1,7 @@
+#Exercise for lists
+#Fruits = ["Apple", "Banana", "Cherry", "Mango" ]
+#Colors = ["Red", "Blue", "Black", "Pink" ]
+#1. Print the third item in the list of Fruits
+#2. Change the value from "apple" to "kiwi" in the Fruits list and print the updated list
+#3. Add "Green" to the list Colors and print the updated list
+#4. Remove "Banana" from the list Fruits and print the updated list
