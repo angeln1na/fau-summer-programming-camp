5be50bdb42ab68e35a2b9d463dae237b6ac3669a
angeln1na <123002085+angeln1na@users.noreply.github.com>
2023-07-14 09:37:59 -0400

    Create forloops_multiplicationtable.py

---
+++ week1/forloops_multiplicationtable.py
@@ -0,0 +1,7 @@
+#Exercise for for loops
+#Make a program that prints the multiplication table of a given number (1, 2, 3,...,10)
+#Get the number from the user
+#Print out a sentence saying what multiplication number will be used
+#Use the for loop multiplying the given number by numbers 1 through 10 and printing each result
+#HINT: range() will be useful, example: range(1,11)
+#Print out each result in this format, for example: 5 x 1 = 5, 5 x 2 = 10, etc.,
