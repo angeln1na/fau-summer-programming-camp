c403cff4b4e2fe4edcd6e864a6206bd9ee876d7b
angeln1na <123002085+angeln1na@users.noreply.github.com>
2023-07-14 09:34:44 -0400

    Create practice5.py

---
+++ week1/practice5.py
@@ -0,0 +1,6 @@
+#1. Write an input statement asking the adventurer a question: "How far are you willing to travel? Enter the number of steps you dare to take."
+#2. Write an input statement to ask the adventurer, "What shall be the name of your wizard companion?"
+#3. Write a line to start a for loop that counts the steps. Remember to use the range() function. Think about what numbers should be inside the parentheses.
+#4. Inside the loop write a print statement a sentence introducing the wizard companion. Remember to include the name of the wizard companion in the sentence.
+#5. Add another line inside the loop to describe each step of the wizard's journey. Make sure to include the step number in the sentence and the name of the wizard companion.
+#6. The loop is complete. Now, write a line of code outside the loop to print a congratulatory message for completing the journey. Remember to include the name of the wizard companion in the message.
