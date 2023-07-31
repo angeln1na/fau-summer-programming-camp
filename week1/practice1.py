738d4cd7cc2b0d368ed87375d291ec83b7b2c95a
angeln1na <123002085+angeln1na@users.noreply.github.com>
2023-07-31 15:43:18 -0400

    Create practice1.py

---
+++ week1/practice1.py
@@ -0,0 +1,8 @@
+# Print statement exercise
+# 1. Write a print statement about where you are from
+# 2. Write a print statement about your favorite food
+# 3. Write a print statement about your favorite song
+# 4. Write an input statement for age and asking the how old they are
+# 5. Write an input statement for hobbies and ask what their hobbies are
+# 6. Write an input statement for travel and ask where they want to go
+# 7. Return all variables in the print statement like in the example
