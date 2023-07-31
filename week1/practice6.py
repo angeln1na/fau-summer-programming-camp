2298eec469cdd84902a4b7fde72bd8b4b2c3d830
angeln1na <123002085+angeln1na@users.noreply.github.com>
2023-07-31 15:49:31 -0400

    Create practice6.py

---
+++ week1/practice6.py
@@ -0,0 +1,27 @@
+#1. Write a line of code to assign a secret number to a variable, like this: secret_number = 7
+
+#2. Print and display a welcoming message to the player. Write a line of code to print a message, for example: print("Welcome to the Guessing Game! Can you guess the secret number?")
+
+#3. Set up a while loop to keep the game running until the player guesses the correct number. Write the initial line of the while loop, like this: while guess != secret_number:
+
+#4. The guess variable will hold the player's guess, which starts with a value of 0 initially.
+
+#5. The loop will continue as long as the player's guess is not equal to the secret number.
+
+#6. Inside the while loop, ask the player to enter their guess. Write a line of code to prompt the player, like this: guess = int(input("Enter your guess: "))
+
+#7. The int() function is used to convert the input into a whole number.
+
+#8. Add conditional statements to provide feedback based on the player's guess. Use if-elif-else statements. For example:
+
+#9. If the guess is lower than the secret number, print "Too low! Try again."
+
+#10. If the guess is higher than the secret number, print "Too high! Try again."
+
+#11. If the guess is equal to the secret number, the loop will end.
+
+#12. Step 7: After the while loop, write a line of code to print a congratulatory message for guessing the secret number correctly. For example: print("Congratulations! You guessed the secret number", secret_number, "correctly!")
+
+#13. Step 8: Save the file with a descriptive name, run it, and play the guessing game!
+
+#14. The program will prompt you to enter a guess and provide feedback until you guess the correct number.
